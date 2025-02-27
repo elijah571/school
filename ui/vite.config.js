53b54cb7f6002dbedd501afcd3ab3217bf6200cb
elijah571 <elijahfx43@gmail.com>
2025-02-27 17:26:12 +0100

    Fix: Specify Node version and adjust package.json for Render

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,19 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173, // Explicitly set the port
     proxy: {
       '/api': {
-        target: 'https://school-backend-umg3.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Remove '/api' prefix if not needed in backend
+        target: "https://school-backend-umg3.onrender.com",  // Target backend server
+        changeOrigin: true,  // This is useful if the backend server is on a different origin
+      
       },
+      
     },
-  },
-  build: {
-    outDir: 'dist', // Output directory for the built files
   },
 });
