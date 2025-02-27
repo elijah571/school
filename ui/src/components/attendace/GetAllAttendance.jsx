05486dd42176bdc8566760e371deee332efd8306
elijah571 <elijahfx43@gmail.com>
2025-02-27 14:12:04 +0100

    removed attendaceList

--- ui/src/components/attendace/GetAllAttendance.jsx
+++ ui/src/components/attendace/GetAllAttendance.jsx
@@ -1,10 +1,10 @@
 import { Attendance } from "../../pages/attendance/Attendance"
-import { AttendanceList } from "./AttendaceList"
+// import { AttendanceList } from "./AttendaceList"
 export const GetAllAttendance = () => {
   return (
     <div>
         <Attendance/>
-        <AttendanceList/>
+        {/* <AttendanceList/> */}
         
     </div>
   )
